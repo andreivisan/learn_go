ce2c6e4beb1af47332ace07c0f06f65fcba1bfb1
Andrei Visan <andrei.m.visan@gmail.com>
2024-11-16 16:57:34 +0100

    Updated docs && tree package

--- tour_of_go/equivalent_binary_trees.go
+++ tour_of_go/equivalent_binary_trees.go
@@ -5,6 +5,34 @@ import (
 	"tour_of_go/tree"
 )
 
+func Walk(t *tree.Tree, ch chan int) {
+	defer close(ch)
+	var walk func(t *tree.Tree)
+	walk = func(t *tree.Tree) {
+		if t == nil {
+			return
+		}
+		walk(t.Left)
+		ch <- t.Value
+		walk(t.Right)
+	}
+	walk(t)
+}
+
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	for i := range ch1 {
+		if i != <-ch2 {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
-	fmt.Println(tree.New(1))
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(1), tree.New(2)))
 }
