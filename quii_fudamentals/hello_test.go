02484ad3740227dcb2dc23d097f3e44a3fd34638
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-11-19 21:58:52 +0100

    Starting TDD from quii

--- quii_fudamentals/hello_test.go
+++ quii_fudamentals/hello_test.go
@@ -3,6 +3,14 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
+  t.Run("say hello to a person", func(t *testing.T) {
+    got := Hello("Andrei")
+    want := "Hello, Andrei"
+
+    if got != want {
+      t.Errorf("got %q want %q", got, want)
+    }
+  })	
 	got := Hello("Andrei")
 	want := "Hello, Andrei"
 
