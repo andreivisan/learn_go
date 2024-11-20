c4b4f53fda152bcdfe58429ec05bd307a59aea82
Andrei Visan^ <andrei.m.visan@gmail.com>
2024-11-20 17:18:12 +0100

    Added example to testing

---
+++ quii_fudamentals/helloworld/hello.go
@@ -0,0 +1,36 @@
+package helloworld
+
+import "fmt"
+
+const (
+  spanish = "Spanish"
+  french = "French"
+  englishHelloPrefix = "Hello, "
+  spanishHelloPrefix = "Hola, "
+  frenchHelloPrefix = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+  if name == "" {
+    name = "World"
+  }
+  return greetingPrefix(language) + name
+}
+
+//In our function signature we have made a named return value (prefix string)
+//This will create a variable called prefix in your function.
+func greetingPrefix(language string) (prefix string) {
+  switch language {
+    case french:
+      prefix = frenchHelloPrefix
+    case spanish:
+      prefix = spanishHelloPrefix
+    default:
+      prefix = englishHelloPrefix
+  }
+  return
+}
+
+func main() {
+	fmt.Println(Hello("Andrei", "English"))
+}
