d1635d84c238423d3410ba7a9caff8315ebb213c
Andrei Visan <andrei.m.visan@gmail.com>
2024-11-30 18:34:43 +0100

    Added Reading Files

--- quii_fudamentals/reading-files/blogposts_test.go
+++ quii_fudamentals/reading-files/blogposts_test.go
@@ -15,36 +15,51 @@ func (s StubFailingFS) Open(name string) (fs.File, error) {
 	return nil, errors.New("oh no, i always fail")
 }
 
-func TestBlogPosts(t *testing.T) {
+func TestNewBlogPosts(t *testing.T) {
+	const (
+		firstBody = `Title: Post 1
+Description: Description 1
+Tags: tdd, go
+---
+Hello
+World`
+		secondBody = `Title: Post 2
+Description: Description 2
+Tags: rust, borrow-checker
+---
+B
+L
+M`
+	)
+
 	fs := fstest.MapFS{
-		"hello world.md": {Data: []byte("Hi")},
-		"hello-world.md": {Data: []byte("Hola")},
+		"hello world.md":  {Data: []byte(firstBody)},
+		"hello-world2.md": {Data: []byte(secondBody)},
 	}
 
 	posts, err := NewPostsFromFS(fs)
 
-	if err != nil {
-		t.Fatal(err)
-	}
+	assertNoError(t, err)
 
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, wanted %d posts", len(posts), len(fs))
+	assertPost(t, posts[0], Post{
+		Title:       "Post 1",
+		Description: "Description 1",
+		Tags:        []string{"tdd", "go"},
+		Body: `Hello
+World`,
+	})
+}
+
+func assertPost(t *testing.T, got Post, want Post) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %+v, want %+v", got, want)
 	}
 }
 
-func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("Title: Post 1")},
-		"hello-world2.md": {Data: []byte("Title: Post 2")},
-	}
-
-	posts, _ := NewPostsFromFS(fs)
-
-	// rest of test code cut for brevity
-	got := posts[0]
-	want := Post{Title: "Post 1"}
-
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %+v, want %+v", got, want)
+func assertNoError(t *testing.T, err error) {
+	t.Helper()
+	if err != nil {
+		t.Fatal(err)
 	}
 }
