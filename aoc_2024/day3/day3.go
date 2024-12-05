822cb9fc0d907b2845f468fbd2d0ed83c8d62c0c
Andrei Visan <andrei.m.visan@gmail.com>
2024-12-05 19:02:09 +0100

    Finished Day 3 Part 2

--- aoc_2024/day3/day3.go
+++ aoc_2024/day3/day3.go
@@ -9,14 +9,32 @@ import (
 )
 
 func Day3_1(fileName string) int {
-    data, err := os.ReadFile(fileName)
-    if err != nil {
-        log.Fatal(err)
-    }
-    content := string(data)
-    re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+    content := getFileContent(fileName)
+    return computeResult(content)
+}
+
+func Day3_2(fileName string) int {
+    content := getFileContent(fileName)
+    sum := 0
+    enabled := true
+    re := regexp.MustCompile(`do\(\)|don't\(\)|mul\((\d+),(\d+)\)`)
     matches := re.FindAllStringSubmatch(content, -1)
-    return computeResult(matches)
+    for _, match := range matches {
+        instruction := match[0]
+        switch instruction {    
+            case "do()":
+                enabled = true
+            case "don't()":
+                enabled = false
+            default:
+                if enabled {
+                    val1, _ := strconv.Atoi(match[1]) 
+                    val2, _ := strconv.Atoi(match[2]) 
+                    sum += val1 * val2 
+                }
+        }
+    }
+    return sum
 }
 
 func getFileContent(fileName string) string {
@@ -27,7 +45,9 @@ func getFileContent(fileName string) string {
     return string(data)
 }
 
-func computeResult(matches [][]string) int {
+func computeResult(content string) int {
+    re := regexp.MustCompile(`mul\((\d+),(\d+)\)`)
+    matches := re.FindAllStringSubmatch(content, -1)
     sum := 0
     for _, match := range matches {
         val1, _ := strconv.Atoi(match[1]) 
@@ -38,5 +58,5 @@ func computeResult(matches [][]string) int {
 }
 
 func main() {
-    fmt.Println(Day3_1("input_d3.txt"))
+    fmt.Println(Day3_2("input_d3.txt"))
 }
